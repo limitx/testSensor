2436e75b39b2ca5148c53558712d668508a89177
limitx <war6303@gmail.com>
2018-04-17 01:46:39 +0800

    2018/04/17 test2 calibration

--- app/src/main/java/gg/work/limitx/myapplication/AccUtils.java
+++ app/src/main/java/gg/work/limitx/myapplication/AccUtils.java
@@ -1,5 +1,10 @@
 package gg.work.limitx.myapplication;
 
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Handler;
 import android.util.Log;
 
@@ -12,47 +17,82 @@ public class AccUtils {
     // ACC algorithm
     private boolean detectStart;
     private static boolean sflag;
-    int[] xyz,prevXYZ,diffXYZ,sqrXYZ,filteredXYZ;
+
     private ArrayList filterX;
     private ArrayList filterY;
     private ArrayList filterZ;
     private static final int filter_size = 5;
     private static final int filter_d_size = filter_size-1;
 
+    private SensorManager mSensorManager;
+    private Sensor mSensor;
+
     private Handler mHandler;
 
-    public void AccUtils(Handler mhandler) {
-        mHandler = mhandler;
-        initParameter();
+    private MotionListener mListener;
+    public interface MotionListener {
+        public void  onMotionChanged(int type);
     }
 
-    private void initParameter() {
+
+    public void AccUtils(Context context, MotionListener listener){ //Handler handler ) {
+        mListener = listener;
+        //mHandler = handler;
+
+        mSensorManager = (SensorManager)context.getSystemService(Context.SENSOR_SERVICE);
+        mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+    }
+
+    public void enableAccSensor(boolean enable) {
         detectStart = false;
         sflag = false;
-        xyz = new int[3];
-        prevXYZ = new int[3];
-        diffXYZ = new int[3];
-        sqrXYZ = new int[3];
-        filteredXYZ  = new int[3];
         filterX = new ArrayList();
         filterY = new ArrayList();
         filterZ = new ArrayList();
+
+        synchronized (this) {
+            if (enable) {
+                mSensorManager.registerListener(mSensorListener, mSensor,
+                        SensorManager.SENSOR_DELAY_UI);
+            } else {
+                mSensorManager.unregisterListener(mSensorListener);
+            }
+        }
     }
 
-    public void detectPulse() {
+    SensorEventListener mSensorListener = new SensorEventListener() {
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+                detectPulse(event);
+            }
+        }
+
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+            // ignore
+        }
+    };
+
+    long time = 0;
+    public void detectPulse(SensorEvent event) {
+        int[] XYZ = new int[3];
+        int[] prevXYZ = new int[3];
+        int[] sqrXYZ = new int[3];
+        int[] sumXYZ  = new int[3];
+        boolean flag = false;
+
         if (!detectStart) {
             detectStart = true;
-            prevXYZ[0] = xyz[0];
-            prevXYZ[1] = xyz[1];
-            prevXYZ[2] = xyz[2];
+            prevXYZ[0] = (int)(event.values[0]);
+            prevXYZ[1] = (int)(event.values[1]);
+            prevXYZ[2] = (int)(event.values[2]);
         } else {
-            diffXYZ[0] = xyz[0] - prevXYZ[0];
-            diffXYZ[1] = xyz[1] - prevXYZ[1];
-            diffXYZ[2] = xyz[2] - prevXYZ[2];
+            XYZ[0] = (int)(event.values[0]) - prevXYZ[0];
+            XYZ[1] = (int)(event.values[0]) - prevXYZ[1];
+            XYZ[2] = (int)(event.values[0]) - prevXYZ[2];
 
-            sqrXYZ[0] = diffXYZ[0] * diffXYZ[0];
-            sqrXYZ[1] = diffXYZ[1] * diffXYZ[1];
-            sqrXYZ[2] = diffXYZ[2] * diffXYZ[2];
+            sqrXYZ[0] = XYZ[0] * XYZ[0];
+            sqrXYZ[1] = XYZ[1] * XYZ[1];
+            sqrXYZ[2] = XYZ[2] * XYZ[2];
 
             if (filterX.size() < filter_size) {
                 filterX.add(sqrXYZ[0]);
@@ -68,43 +108,45 @@ public class AccUtils {
                 filterZ.add(sqrXYZ[2]);
             }
 
-            int[] sumXYZ  = new int[3];
             for(int i = 1; i < filterX.size(); i++) {
                 sumXYZ[0] += (int)filterX.get(i);
                 sumXYZ[1] += (int)filterY.get(i);
                 sumXYZ[2] += (int)filterZ.get(i);
             }
 
-            filteredXYZ[0] = sumXYZ[0] / filter_d_size; // divide by filter_d_size
-            filteredXYZ[1] = sumXYZ[1] / filter_d_size; // divide by filter_d_size
-            filteredXYZ[2] = sumXYZ[2] / filter_d_size; // divide by filter_d_size
+            XYZ[0] = sumXYZ[0] / filter_d_size; // divide by filter_d_size
+            XYZ[1] = sumXYZ[1] / filter_d_size; // divide by filter_d_size
+            XYZ[2] = sumXYZ[2] / filter_d_size; // divide by filter_d_size
 
             //Log.i(tag, "detectPulse+ " + filteredXYZ[0] +" / "+filteredXYZ[2]);
-            boolean flag = false;
-            if (Math.abs(filteredXYZ[0] - sqrXYZ[0]) > 20 || Math.abs(filteredXYZ[2] - sqrXYZ[2]) > 15) {
-                flag = true;
-                if (sflag != flag) {
-                    sflag = flag;
+
+            if (Math.abs(XYZ[0] - sqrXYZ[0]) > 15 || Math.abs(XYZ[2] - sqrXYZ[2]) > 10) {
+                if (System.currentTimeMillis() - time > 160) {
                     if (mHandler.obtainMessage(1) != null) {
                         mHandler.removeMessages(1);
                     }
                     mHandler.sendEmptyMessage(1);
+
+                    //mListener.onMotionChanged(1);
+
+                    time = System.currentTimeMillis();
                 }
             } else {//if (filteredXYZ[0] < 10 && filteredXYZ[2] < 10) {
-                flag = false;
-                if (sflag != flag) {
-                    sflag = flag;
-                    if (mHandler.obtainMessage(2) != null) {
-                        mHandler.removeMessages(2);
-                    }
-                    mHandler.sendEmptyMessage(2);
+                if(mHandler.obtainMessage(2) != null) {
+                    mHandler.removeMessages(2);
                 }
+                mHandler.sendEmptyMessage(2);
+
+                //mListener.onMotionChanged(2);
+
+                time = System.currentTimeMillis();
+                flag = false;
             }
 
             Log.i(tag, "detectPulse++++ "+flag+" : "+
-                    sqrXYZ[0]+" / "+filteredXYZ[0] +" , "+
-                    sqrXYZ[1]+" / "+filteredXYZ[1]+" , "+
-                    sqrXYZ[2]+" / "+filteredXYZ[2]);
+                    sqrXYZ[0]+" / "+XYZ[0] +" , "+
+                    sqrXYZ[1]+" / "+XYZ[1]+" , "+
+                    sqrXYZ[2]+" / "+XYZ[2]);
         }
     }
 }

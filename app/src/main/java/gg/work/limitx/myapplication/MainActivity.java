83f016546b84c048946a9a9bc49458ac40e51a06
minche_li <war6303@gmail.com>
2018-04-16 16:44:25 +0800

    2018/04/16 test

--- app/src/main/java/gg/work/limitx/myapplication/MainActivity.java
+++ app/src/main/java/gg/work/limitx/myapplication/MainActivity.java
@@ -11,6 +11,8 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
@@ -19,29 +21,57 @@ import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements SensorEventListener {
 
     private final String tag = "MainActivity";
+
+    // Sensors
     private SensorManager mSensorManager;
     private Sensor mACC;
     private Sensor mGYR;
     private Sensor mROTATE;
 
-
+    // Canvas waveform plot
+    private SurfaceView sfv;
+    private SurfaceHolder sfh;
+    private static final int offset1 = 500;
+    private static final int offset2 = 800;
+    private Paint mPaint1 = new Paint();
+    private Paint mPaint2 = new Paint();
+    private Paint mPaint3 = new Paint();
+    private Paint mPaint4 = new Paint();
+    private Paint mPaint5 = new Paint();
+    private Paint mPaint6 = new Paint();
+    private Paint mPaintBlack = new Paint();
+    private Canvas canvas;
+    int xx,oldY,oldY1,oldY2,oldY3,oldY4,oldY5;
+    // Screen size for Canvas
     int Height,Width;
 
-    boolean dflag = false;
-    SurfaceView sfv;
-    SurfaceHolder sfh;
-    Paint mPaint1 = new Paint();
-    Paint mPaint2 = new Paint();
-    Paint mPaint3 = new Paint();
-    Paint mPaint4 = new Paint();
-    Canvas canvas;
-    int xx=1,oldY=0, oldY1=0 , oldY2=0 , oldY3=0 ;
+
+    TextView tv1;
+
+
+    // for other Sensors
+    final float alpha = (float) 0.8;
+    float[] gravity = new float[3];
+    float[] linear_acceleration = new float[3];
+
+
+    // ACC algorithm
+    private boolean detectStart = false;
+    int[] xyz,prevXYZ,diffXYZ,sqrXYZ,filteredXYZ;
+    private ArrayList filterX = new ArrayList();
+    private ArrayList filterZ = new ArrayList();
+    private static final int filter_size = 5;
+    private static final int filter_d_size = filter_size-1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,12 +80,13 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
         mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
+        //List<Sensor> deviceSensors = mSensorManager.getSensorList(Sensor.TYPE_ALL);
         mACC = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);//TYPE_LINEAR_ACCELERATION
         mGYR = mSensorManager.getDefaultSensor(Sensor.TYPE_GYROSCOPE);
         mROTATE = mSensorManager.getDefaultSensor(Sensor.TYPE_ROTATION_VECTOR);
 
 
+        // Canvas waveform plot
         DisplayMetrics metrics = new DisplayMetrics();
         getWindowManager().getDefaultDisplay().getMetrics(metrics);
         Height = (int)(metrics.heightPixels * 0.5);
@@ -68,18 +99,25 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         mPaint1.setColor(Color.GREEN);
         mPaint1.setStrokeWidth(3);
         mPaint2.setColor(Color.RED);
-        mPaint2.setStrokeWidth(5);
+        mPaint2.setStrokeWidth(3);
         mPaint3.setColor(Color.BLUE);
         mPaint3.setStrokeWidth(3);
-        mPaint4.setColor(Color.BLACK);
+        mPaint4.setColor(Color.WHITE);
+        mPaint4.setStrokeWidth(3);
+        mPaint5.setColor(Color.YELLOW);
+        mPaint5.setStrokeWidth(3);
+        mPaint6.setColor(Color.CYAN);
+        mPaint6.setStrokeWidth(2);
+        mPaintBlack.setColor(Color.BLACK);
+
+
+        tv1 = findViewById(R.id.textView);
 
         Button bt1 = findViewById(R.id.bt1);
         bt1.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //dflag = !dflag;
-                drawthread();
-                //Log.i(tag,"onclick" + dflag);
+                drawThread();
             }
         });
     }
@@ -87,9 +125,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     @Override
     protected void onResume() {
         super.onResume();
-        mSensorManager.registerListener(this, mACC, SensorManager.SENSOR_DELAY_UI);
+
+        initParameter();
+        mSensorManager.registerListener(this, mACC, SensorManager.SENSOR_DELAY_UI);//25Hz
+        //mSensorManager.registerListener(this, mACC, SensorManager.SENSOR_DELAY_GAME);//100Hz
+
+        /*mSensorManager.registerListener(this, mACC, SensorManager.SENSOR_DELAY_UI);
         mSensorManager.registerListener(this, mGYR, SensorManager.SENSOR_DELAY_GAME);
-        mSensorManager.registerListener(this, mROTATE, SensorManager.SENSOR_DELAY_GAME);
+        mSensorManager.registerListener(this, mROTATE, SensorManager.SENSOR_DELAY_GAME);*/
+
+
+        tv1.setText(mACC.getName() +"  width:"+ Width);
     }
 
     @Override
@@ -107,7 +153,6 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     @Override
     public final void onSensorChanged(SensorEvent event) {
-
         if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
             showSensorData(event);
         }/* else if (event.sensor.getType() == Sensor.TYPE_GYROSCOPE) {
@@ -117,10 +162,33 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         }*/
     }
 
-    final float alpha = (float) 0.8;
-    float[] gravity = new float[3];
-    float[] linear_acceleration = new float[3];
-    int[] xyz = new int[3];
+    Toast toastMessage;
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch(msg.what){
+                case 1:
+                    if (toastMessage != null) {
+                        toastMessage.cancel();
+                    }
+                    toastMessage = Toast.makeText(MainActivity.this, "motion detected!!",Toast.LENGTH_SHORT);
+                    toastMessage.show();
+
+                    tv1.setText(filteredXYZ[0] +" / "+filteredXYZ[2]);
+                    tv1.setTextColor(Color.RED);
+                    Log.i(tag, "toastMessage++");
+                    break;
+                case 2:
+                    if (toastMessage != null) {
+                        toastMessage.cancel();
+                    }
+                    tv1.setTextColor(Color.BLACK);
+                    Log.i(tag, "toastMessage--");
+                    break;
+            }
+        }
+    };
+
     private void showSensorData(SensorEvent event) {
 /*
         // In this example, alpha is calculated as t / (t + dT),
@@ -137,27 +205,23 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         linear_acceleration[1] = event.values[1] - gravity[1];
         linear_acceleration[2] = event.values[2] - gravity[2];
 */
-        xyz[0] = (int)(event.values[0]*20)+offset;
-        xyz[1] = (int)(event.values[1]*20)+offset;
-        xyz[2] = (int)(event.values[2]*20)+offset;
-        Log.i(tag, event.sensor.getType() +" xyz: "+xyz);
-        /*int x1=5,y1=7,z1=9;
-        draw3(x1,y1,z1);*/
-        /*if (dflag) {
-            draw3((int)(linear_acceleration[0]*20),(int)(linear_acceleration[1]*20),(int)(linear_acceleration[2]*20));
-        }*/
+        xyz[0] = (int)(event.values[0]);
+        xyz[1] = (int)(event.values[1]);
+        xyz[2] = (int)(event.values[2]);
+        //Log.i(tag, "showSensorData+ "+ xyz.toString());
     }
 
 
-    private void drawthread() {
+    private void drawThread() {
         new Thread() {
             @Override
             public void run() {
                 super.run();
                 while(true) {
+                    detectPulse();
                     drawAll();
                     try {
-                        Thread.sleep(2);
+                        Thread.sleep(1);
                     } catch (InterruptedException exception) {
 
                     }
@@ -167,19 +231,17 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     }
 
 
-
-    int offset = 150;
     private void draw3(int data1,int data2,int data3) {
         xx++;
-        Log.i(tag, "draw+ "+xx+"  "+data1+" "+data2+" "+data3);
+        //Log.i(tag, "draw+ "+xx+"  "+data1+" "+data2+" "+data3);
         canvas = sfh.lockCanvas(new Rect(xx-1, 0, xx, Height));
-        canvas.drawLine(xx-1, oldY1, xx, data1+offset , mPaint1);
-        canvas.drawLine(xx-1, oldY2, xx, data2+offset , mPaint2);
-        canvas.drawLine(xx-1, oldY3, xx, kalmanFilter(data3+offset) , mPaint3);
+        canvas.drawLine(xx-1, oldY1, xx, offset1-data1 , mPaint1);
+        canvas.drawLine(xx-1, oldY2, xx, offset1-data2 , mPaint2);
+        canvas.drawLine(xx-1, oldY3, xx, offset1-data3 , mPaint3);
 
-        oldY1 = data1+offset;
-        oldY2 = data2+offset;
-        oldY3 = data3+offset;
+        oldY1 = offset1-data1;
+        oldY2 = offset1-data2;
+        oldY3 = offset1-data3;
 
         sfh.unlockCanvasAndPost(canvas);
 
@@ -193,36 +255,116 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     private void drawAll() {
         xx++;
-        Log.i(tag, "drawAll+ ");
         canvas = sfh.lockCanvas(new Rect(xx-1, 0, xx, Height));
         if (canvas != null) {
-            canvas.drawLine(xx - 1, oldY1, xx, xyz[0], mPaint1);
-            canvas.drawLine(xx - 1, oldY2, xx, xyz[1], mPaint2);
-            canvas.drawLine(xx - 1, oldY3, xx, xyz[2], mPaint3);
+/*
+            canvas.drawLine(xx - 1, oldY1, xx, offset1 - xyz[0], mPaint1);
+            canvas.drawLine(xx - 1, oldY2, xx, offset1 - xyz[1], mPaint2);
+            canvas.drawLine(xx - 1, oldY3, xx, offset1 - xyz[2], mPaint3);
+            oldY1 = offset1 - xyz[0];
+            oldY2 = offset1 - xyz[1];
+            oldY3 = offset1 - xyz[2];
+*/
+            canvas.drawLine(xx - 1, oldY1, xx,offset1 - sqrXYZ[0], mPaint1);
+            canvas.drawLine(xx - 1, oldY2, xx,offset1 - sqrXYZ[1], mPaint2);
+            canvas.drawLine(xx - 1, oldY3, xx,offset1 - sqrXYZ[2], mPaint3);
+            oldY1 = offset1 - sqrXYZ[0];
+            oldY2 = offset1 - sqrXYZ[1];
+            oldY3 = offset1 - sqrXYZ[2];
 
-            oldY1 = xyz[0];
-            oldY2 = xyz[1];
-            oldY3 = xyz[2];
+            //++++
+            canvas.drawLine(xx - 1, oldY4, xx,offset2 - filteredXYZ[0], mPaint4);
+            canvas.drawLine(xx - 1, oldY5, xx,offset2 - filteredXYZ[2], mPaint5);
+            canvas.drawLine(0, offset2-50, xx, offset2-50 , mPaint6);
+            oldY4 = offset2 - filteredXYZ[0];
+            oldY5 = offset2 - filteredXYZ[2];
 
             sfh.unlockCanvasAndPost(canvas);
+
+            //++++Log.i(tag, "draw+ "+xx+"  "+sqrXYZ[0]+" "+sqrXYZ[1]+" "+sqrXYZ[2]);
         }
 
         if (xx>=Width) {
             xx=0; //canvas.drawColor(Color.TRANSPARENT,Mode.CLEAR);
             canvas = sfh.lockCanvas(new Rect(0, 0, Width, Height));
-            canvas.drawRect(0, 0, Width, Height , mPaint4);
+            if (canvas != null) {
+                canvas.drawRect(0, 0, Width, Height, mPaintBlack);
+            }
             sfh.unlockCanvasAndPost(canvas);
         }
     }
 
-    static double prevData=0,p=12, q=0.003, r=0.1, kGain=0;
-    int kalmanFilter(int inData) {
-        p += q;
-        kGain = p/(p+r);
-        inData = (int)(prevData+(kGain*(((double)(inData)-prevData))));
-        p *= (1-kGain);
-        prevData = inData;
+    private void initParameter() {
+        detectStart = false;
+        xyz = new int[3];
+        prevXYZ = new int[3];
+        diffXYZ = new int[3];
+        sqrXYZ = new int[3];
+        filteredXYZ  = new int[3];
+        filterX = new ArrayList();
+        filterZ = new ArrayList();
 
-        return inData;
+        xx=1;
+        oldY=0;
+        oldY1=0;
+        oldY2=0;
+        oldY3=0;
+        oldY4=0;
+        oldY5=0;
+    }
+
+    private void detectPulse() {
+        if (!detectStart) {
+            detectStart = true;
+            prevXYZ[0] = xyz[0];
+            prevXYZ[1] = xyz[1];
+            prevXYZ[2] = xyz[2];
+        } else {
+            diffXYZ[0] = xyz[0] - prevXYZ[0];
+            diffXYZ[1] = xyz[1] - prevXYZ[1];
+            diffXYZ[2] = xyz[2] - prevXYZ[2];
+
+            sqrXYZ[0] = diffXYZ[0] * diffXYZ[0];
+            sqrXYZ[1] = diffXYZ[1] * diffXYZ[1];
+            sqrXYZ[2] = diffXYZ[2] * diffXYZ[2];
+
+           if (filterX.size() < filter_size) {
+                filterX.add(sqrXYZ[0]);
+                filterZ.add(sqrXYZ[2]);
+           } else {
+                //Log.i(tag, "detectPulse+ "+filter.size() +" "+sqrXYZ[0]+" / "+filter.toString());
+                filterX.remove(0);
+                filterX.add(sqrXYZ[0]);
+                filterZ.remove(2);
+                filterZ.add(sqrXYZ[2]);
+           }
+
+           int sumX = 0, sumZ = 0;
+           for(int i = 1; i < filterX.size(); i++) {
+               sumX += (int)filterX.get(i);
+               sumZ += (int)filterZ.get(i);
+           }
+
+           filteredXYZ[0] = sumX / filter_d_size; // divide by filter_d_size
+           filteredXYZ[2] = sumZ / filter_d_size; // divide by filter_d_size
+
+           //Log.i(tag, "detectPulse+ " + filteredXYZ[0] +" / "+filteredXYZ[2]);
+           boolean flag = false;
+           if (Math.abs(filteredXYZ[0] - sqrXYZ[0]) > 15 || Math.abs(filteredXYZ[0] - sqrXYZ[0]) > 10) {
+               if(mHandler.obtainMessage(1) != null) {
+                   mHandler.removeMessages(1);
+               }
+               mHandler.sendEmptyMessage(1);
+               flag = true;
+           } else {//if (filteredXYZ[0] < 10 && filteredXYZ[2] < 10) {
+               if(mHandler.obtainMessage(2) != null) {
+                   mHandler.removeMessages(2);
+               }
+               mHandler.sendEmptyMessage(2);
+               flag = false;
+           }
+
+           Log.i(tag, "detectPulse++++ "+flag+" : "+ sqrXYZ[0]+" / "+filteredXYZ[0] +" , "+ sqrXYZ[2]+" / "+filteredXYZ[2]);
+        }
     }
 }
